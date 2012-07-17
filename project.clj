e79afa59ff4c9d23783a3217117e48992688c1f4
puredanger <alexdmiller@yahoo.com>
2012-07-17 07:57:08 -0500

    hacks to make aot-compiled clojure 1.4 version work

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject slingshot "0.10.3-revelytix"
+(defproject slingshot "0.10.3-revelytix1"
   :description "Enhanced throw, try, leveraging Clojure's capabilities"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :java-source-path "java/src"
   :warn-on-reflection true)
